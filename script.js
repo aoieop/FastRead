6137006b5e6f089ae5931564f53ba548afc5eec4
aoieop <austincui2007@gmail.com>
2026-01-19 12:29:36 -0500

    Website Commit

---
+++ script.js
@@ -0,0 +1,49 @@
+{
+    const input = document.getElementById("inp");
+    const output = document.getElementById("tex");
+    const start = document.getElementById("start");
+    const stop = document.getElementById("stop");
+    const peem = document.getElementById("wpm");
+
+    var inp = "";
+    var word = 0;
+    var words;
+    var on = false;
+    var timer;
+    var speed = 120;
+
+    function nextWord(){
+            word++;
+            output.textContent = words[word];
+    }
+
+    function turnOn(){
+        timer = setInterval(nextWord, speed);
+    }
+
+    function turnOff(){
+        clearInterval(timer);
+    }
+
+    peem.addEventListener("click", turnOff);
+    peem.addEventListener("input", ()=>{
+        speed = 60000/parseInt(peem.value);
+    })
+    
+    input.addEventListener("click", turnOff);
+
+    
+    input.addEventListener("input", () => {
+
+        word = 0;
+        inp = input.value;
+        words = inp.split(" ");
+        output.textContent = words[word];
+    });
+
+    start.addEventListener("click", turnOn);
+    stop.addEventListener("click", turnOff);
+
+    
+
+}
